be746f8db4f7cf6a561495df4eb8b17e56439e5f
Stepan <stepos36@gmail.com>
2019-01-29 23:24:57 -0500

    jquery circulate plugin added// media queries for animated content

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,359 @@
+$(document).ready(function() {
+    var mq1 = window.matchMedia( "(max-width: 1198px)" );
+    var mq1min = window.matchMedia( "(min-width: 993px)" );
+    var mq2 = window.matchMedia( "(max-width: 992px)" );
+    var mq2min = window.matchMedia( "(min-width: 769px)" );
+    var mq3 = window.matchMedia( "(max-width: 768px)" );
+    var mq3min = window.matchMedia( "(min-width: 571px)" );
+    var mq4 = window.matchMedia( "(max-width: 570px)" );
+    if ((mq1.matches)&&(mq1min.matches)) {
+        $(".html5").css("left", $(".html5").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -320,
+            height: 50,
+            loop: true,
+            zIndexValues: [8, 49, 49, 8]
+        });
+        setTimeout(function() {
+        $(".css3").css("left", $(".css3").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -320,
+            height: 50,
+            loop: true,
+            zIndexValues: [7, 50, 50, 7]
+        });
+        },1150)
+        setTimeout(function() {
+        $(".bootstrap").css("left", $(".bootstrap").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -320,
+            height: 50,
+            loop: true,
+            zIndexValues: [6, 51, 51, 6]
+        });
+        },2300)
+        setTimeout(function() {
+        $(".javascript").css("left", $(".javascript").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -320,
+            height: 50,
+            loop: true,
+            zIndexValues: [5, 52, 52, 5]
+        });
+        },3450)
+        setTimeout(function() {
+        $(".node").css("left", $(".node").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -320,
+            height: 50,
+            loop: true,
+            zIndexValues: [4, 53, 53, 4]
+        });
+        },4600)
+        setTimeout(function() {
+        $(".express").css("left", $(".express").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -320,
+            height: 50,
+            loop: true,
+            zIndexValues: [3, 54, 54, 3]
+        });
+        },5750)
+        setTimeout(function() {
+        $(".mongo").css("left", $(".mongo").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -320,
+            height: 50,
+            loop: true,
+            zIndexValues: [2, 55, 55, 2]
+        });
+        },6700)
+        }
+    if ((mq2.matches)&&(mq2min.matches)) {
+        $(".html5").css("left", $(".html5").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -270,
+            height: 20,
+            loop: true,
+            zIndexValues: [8, 49, 49, 8]
+        });
+        setTimeout(function() {
+        $(".css3").css("left", $(".css3").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -270,
+            height: 20,
+            loop: true,
+            zIndexValues: [7, 50, 50, 7]
+        });
+        },1150)
+        setTimeout(function() {
+        $(".bootstrap").css("left", $(".bootstrap").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -270,
+            height: 20,
+            loop: true,
+            zIndexValues: [6, 51, 51, 6]
+        });
+        },2300)
+        setTimeout(function() {
+        $(".javascript").css("left", $(".javascript").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -270,
+            height: 20,
+            loop: true,
+            zIndexValues: [5, 52, 52, 5]
+        });
+        },3450)
+        setTimeout(function() {
+        $(".node").css("left", $(".node").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -270,
+            height: 20,
+            loop: true,
+            zIndexValues: [4, 53, 53, 4]
+        });
+        },4600)
+        setTimeout(function() {
+        $(".express").css("left", $(".express").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -270,
+            height: 20,
+            loop: true,
+            zIndexValues: [3, 54, 54, 3]
+        });
+        },5750)
+        setTimeout(function() {
+        $(".mongo").css("left", $(".mongo").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -270,
+            height: 20,
+            loop: true,
+            zIndexValues: [2, 55, 55, 2]
+        });
+        },6700)
+    }
+    if ((mq3.matches)&&(mq3min.matches)) {
+        $(".html5").css("left", $(".html5").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -300,
+            height: 10,
+            loop: true,
+            zIndexValues: [8, 49, 49, 8]
+        });
+        setTimeout(function() {
+        $(".css3").css("left", $(".css3").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -300,
+            height: 10,
+            loop: true,
+            zIndexValues: [7, 50, 50, 7]
+        });
+        },1150)
+        setTimeout(function() {
+        $(".bootstrap").css("left", $(".bootstrap").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -300,
+            height: 10,
+            loop: true,
+            zIndexValues: [6, 51, 51, 6]
+        });
+        },2300)
+        setTimeout(function() {
+        $(".javascript").css("left", $(".javascript").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -300,
+            height: 10,
+            loop: true,
+            zIndexValues: [5, 52, 52, 5]
+        });
+        },3450)
+        setTimeout(function() {
+        $(".node").css("left", $(".node").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -300,
+            height: 10,
+            loop: true,
+            zIndexValues: [4, 53, 53, 4]
+        });
+        },4600)
+        setTimeout(function() {
+        $(".express").css("left", $(".express").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -300,
+            height: 10,
+            loop: true,
+            zIndexValues: [3, 54, 54, 3]
+        });
+        },5750)
+        setTimeout(function() {
+        $(".mongo").css("left", $(".mongo").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -300,
+            height: 10,
+            loop: true,
+            zIndexValues: [2, 55, 55, 2]
+        });
+        },6700)
+    }
+    if (mq4.matches) {
+        $(".html5").css("left", $(".html5").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -180,
+            height: 5,
+            loop: true,
+            zIndexValues: [8, 49, 49, 8]
+        });
+        setTimeout(function() {
+        $(".css3").css("left", $(".css3").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -180,
+            height: 5,
+            loop: true,
+            zIndexValues: [7, 50, 50, 7]
+        });
+        },1150)
+        setTimeout(function() {
+        $(".bootstrap").css("left", $(".bootstrap").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -180,
+            height: 5,
+            loop: true,
+            zIndexValues: [6, 51, 51, 6]
+        });
+        },2300)
+        setTimeout(function() {
+        $(".javascript").css("left", $(".javascript").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -180,
+            height: 5,
+            loop: true,
+            zIndexValues: [5, 52, 52, 5]
+        });
+        },3450)
+        setTimeout(function() {
+        $(".node").css("left", $(".node").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -180,
+            height: 5,
+            loop: true,
+            zIndexValues: [4, 53, 53, 4]
+        });
+        },4600)
+        setTimeout(function() {
+        $(".express").css("left", $(".express").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -180,
+            height: 5,
+            loop: true,
+            zIndexValues: [3, 54, 54, 3]
+        });
+        },5750)
+        setTimeout(function() {
+        $(".mongo").css("left", $(".mongo").position().left).circulate({
+            sizeAdjustment: 160,
+            speed: 1900,
+            width: -180,
+            height: 5,
+            loop: true,
+            zIndexValues: [2, 55, 55, 2]
+        });
+        },6700)
+    }
+    else{
+$(".html5").css("left", $(".html5").position().left).circulate({
+    sizeAdjustment: 160,
+    speed: 1900,
+    width: -420,
+    height: 50,
+    loop: true,
+    zIndexValues: [8, 49, 49, 8]
+});
+setTimeout(function() {
+$(".css3").css("left", $(".css3").position().left).circulate({
+    sizeAdjustment: 160,
+    speed: 1900,
+    width: -420,
+    height: 50,
+    loop: true,
+    zIndexValues: [7, 50, 50, 7]
+});
+},1150)
+setTimeout(function() {
+$(".bootstrap").css("left", $(".bootstrap").position().left).circulate({
+    sizeAdjustment: 160,
+    speed: 1900,
+    width: -420,
+    height: 50,
+    loop: true,
+    zIndexValues: [6, 51, 51, 6]
+});
+},2300)
+setTimeout(function() {
+$(".javascript").css("left", $(".javascript").position().left).circulate({
+    sizeAdjustment: 160,
+    speed: 1900,
+    width: -420,
+    height: 50,
+    loop: true,
+    zIndexValues: [5, 52, 52, 5]
+});
+},3450)
+setTimeout(function() {
+$(".node").css("left", $(".node").position().left).circulate({
+    sizeAdjustment: 160,
+    speed: 1900,
+    width: -420,
+    height: 50,
+    loop: true,
+    zIndexValues: [4, 53, 53, 4]
+});
+},4600)
+setTimeout(function() {
+$(".express").css("left", $(".express").position().left).circulate({
+    sizeAdjustment: 160,
+    speed: 1900,
+    width: -420,
+    height: 50,
+    loop: true,
+    zIndexValues: [3, 54, 54, 3]
+});
+},5750)
+setTimeout(function() {
+$(".mongo").css("left", $(".mongo").position().left).circulate({
+    sizeAdjustment: 160,
+    speed: 1900,
+    width: -420,
+    height: 50,
+    loop: true,
+    zIndexValues: [2, 55, 55, 2]
+});
+},6700)
+}
+})
